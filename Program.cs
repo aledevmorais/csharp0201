c48dc536e2c984f9b4dd9d1e4ab0ac72bf7914c9
alede <alede@ALESHARP>
2023-01-30 08:47:43 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Alura0201;
+
+contaCorrente contadoAndre = new contaCorrente();
+contadoAndre.titular = "André Silva";
+contadoAndre.numero_agencia = 15;
+contadoAndre.conta = "1011-X";
+contadoAndre.saldo = 100;
+
+Console.WriteLine("Saldo da conta do Andre:  " + contadoAndre.saldo);
---
+++ contaCorrente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura0201
+{
+    public class contaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+    }
+}
