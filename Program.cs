f96068cc00802c09488b9007667d2d8ad21f0e0f
aledemorais <aledevmorais@gmail.com>
2023-01-30 12:27:36 -0300

    saque e deposito

--- Program.cs
+++ Program.cs
@@ -11,3 +11,26 @@ Console.WriteLine("Saldo da conta do Andre:  " + contadoAndre.saldo);
 contadoAndre.depositar(100);
 
 Console.WriteLine("Saldo da conta do Andre Atual :  " + contadoAndre.saldo);
+
+if (contadoAndre.sacar(300)== true)
+{
+    Console.WriteLine("Saldo da conta do Andre Atual pós saque :  " + contadoAndre.saldo);
+}
+else
+{
+    Console.WriteLine("Saldo insuficente para esta operação de saque");
+}
+
+contaCorrente contaDaFulana = new contaCorrente();
+contaDaFulana.titular = "Yvonne Strahovski";
+contaDaFulana.numero_agencia = 17;
+contaDaFulana.conta = "100200 - 1";
+contaDaFulana.saldo = 350;
+
+Console.WriteLine("Saldo da Conta da Yvonne : " + contaDaFulana.saldo);
+
+contadoAndre.transferir(50, contaDaFulana);
+Console.WriteLine("saldo da conta do André : " + contadoAndre.saldo);
+Console.WriteLine("saldo da conta da Yvonne : " + contaDaFulana.saldo);
+
+
--- contaCorrente.cs
+++ contaCorrente.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +17,33 @@ namespace Alura0201
         public void depositar(double valor)
         {
             this.saldo += valor;
-        }
 
+        }
+        public bool sacar(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool transferir(double valor, contaCorrente destino)
+        {
+            if (this.saldo <  valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.sacar(valor);
+                destino.depositar(valor);
+                return true;
+
+            }
+        }
     }
 }
